35c9117b309f91a921649796e511e65d06997e44
tt0410tt <tt0410tt@naver.com>
2024-10-24 15:21:14 +0900

    코드리펙토링 임시저장4

--- repectoring_project/nlptutti/asr_metrics.py
+++ repectoring_project/nlptutti/asr_metrics.py
@@ -1,75 +1,12 @@
 from typing import Any, Dict, List, Tuple
 import jiwer
 from collections import OrderedDict
+from nlptutti import utils
 
 class ASRMetrics:
     """
     음성 인식 결과의 오류율(CER, WER, CRR)을 계산하는 클래스입니다.
     """
-
-    def __init__(self):
-        pass
-
-    def levenshtein(self, u: List[Any], v: List[Any]) -> Tuple[int, Tuple[int, int, int]]:
-        """
-        레벤슈타인 거리를 계산하고 대체, 삽입, 삭제 연산의 수를 반환합니다.
-        """
-        prev = None
-        curr = [0] + list(range(1, len(v) + 1))
-        # 연산: (대체, 삽입, 삭제)
-        prev_ops = None
-        curr_ops = [(0, 0, i) for i in range(len(v) + 1)]
-        for x in range(1, len(u) + 1):
-            prev, curr = curr, [x] + ([None] * len(v))
-            prev_ops, curr_ops = curr_ops, [(0, x, 0)] + ([None] * len(v))
-            for y in range(1, len(v) + 1):
-                delcost = prev[y] + 1
-                addcost = curr[y - 1] + 1
-                subcost = prev[y - 1] + int(u[x - 1] != v[y - 1])
-                curr[y] = min(subcost, delcost, addcost)
-                if curr[y] == subcost:
-                    (n_s, n_d, n_i) = prev_ops[y - 1]
-                    curr_ops[y] = (n_s + int(u[x - 1] != v[y - 1]), n_d, n_i)
-                elif curr[y] == delcost:
-                    (n_s, n_d, n_i) = prev_ops[y]
-                    curr_ops[y] = (n_s, n_d + 1, n_i)
-                else:
-                    (n_s, n_d, n_i) = curr_ops[y - 1]
-                    curr_ops[y] = (n_s, n_d, n_i + 1)
-        return curr[len(v)], curr_ops[len(v)]
-
-    def _measure_er(self, reference: str, transcription: str, level: str) -> Tuple[int, int, int, int]:
-        """
-        오류율 계산에 필요한 대체, 삭제, 삽입, 일치의 수를 반환합니다.
-        """
-        ref_list = [reference]
-        hyp_list = [transcription]
-
-        total_s, total_i, total_d, total_n = 0, 0, 0, 0
-
-        for n in range(len(ref_list)):
-            if level == 'char':
-                ref_tokens = list(ref_list[n])
-                hyp_tokens = list(hyp_list[n])
-            elif level == 'word':
-                ref_tokens = ref_list[n].split()
-                hyp_tokens = hyp_list[n].split()
-            else:
-                raise ValueError("Invalid level specified. Choose 'char' or 'word'.")
-
-            _, (s, i, d) = self.levenshtein(hyp_tokens, ref_tokens)
-            total_s += s
-            total_i += i
-            total_d += d
-            total_n += len(ref_tokens)
-
-        substitutions = total_s
-        deletions = total_d
-        insertions = total_i
-        hits = total_n - (substitutions + deletions)
-
-        return hits, substitutions, deletions, insertions
-
     def get_er(self, reference: str, transcription: str, metric: str, rm_punctuation: bool = True) -> Dict[str, Any]:
         """
         오류율(CER, WER, CRR)을 계산하여 결과를 반환합니다.
@@ -106,7 +43,7 @@ class ASRMetrics:
             level = 'word'
 
         # 오류 측정
-        hits, substitutions, deletions, insertions = self._measure_er(refs, trans, level)
+        hits, substitutions, deletions, insertions = utils.measure_er(refs, trans, level)
 
         incorrect = substitutions + deletions + insertions
         total = substitutions + deletions + hits + insertions
--- repectoring_project/nlptutti/utils.py
+++ repectoring_project/nlptutti/utils.py
@@ -3,55 +3,33 @@
 from typing import List, Tuple
 import unicodedata
 
-def levenshtein_distance(ref: List[str], hyp: List[str]) -> Tuple[int, Tuple[int, int, int]]:
+def levenshtein(self, u: List[Any], v: List[Any]) -> Tuple[int, Tuple[int, int, int]]:
     """
     레벤슈타인 거리를 계산하고 대체, 삽입, 삭제 연산의 수를 반환합니다.
     """
-    m = len(ref)
-    n = len(hyp)
-
-    distance_matrix = [[0] * (n + 1) for _ in range(m + 1)]
-    op_matrix = [[(0, 0, 0)] * (n + 1) for _ in range(m + 1)]  # (substitutions, insertions, deletions)
-
-    for i in range(m + 1):
-        distance_matrix[i][0] = i
-        if i > 0:
-            op_matrix[i][0] = (0, 0, i)
-    for j in range(n + 1):
-        distance_matrix[0][j] = j
-        if j > 0:
-            op_matrix[0][j] = (0, j, 0)
-
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if ref[i - 1] == hyp[j - 1]:
-                cost = 0
+    prev = None
+    curr = [0] + list(range(1, len(v) + 1))
+    # 연산: (대체, 삽입, 삭제)
+    prev_ops = None
+    curr_ops = [(0, 0, i) for i in range(len(v) + 1)]
+    for x in range(1, len(u) + 1):
+        prev, curr = curr, [x] + ([None] * len(v))
+        prev_ops, curr_ops = curr_ops, [(0, x, 0)] + ([None] * len(v))
+        for y in range(1, len(v) + 1):
+            delcost = prev[y] + 1
+            addcost = curr[y - 1] + 1
+            subcost = prev[y - 1] + int(u[x - 1] != v[y - 1])
+            curr[y] = min(subcost, delcost, addcost)
+            if curr[y] == subcost:
+                (n_s, n_d, n_i) = prev_ops[y - 1]
+                curr_ops[y] = (n_s + int(u[x - 1] != v[y - 1]), n_d, n_i)
+            elif curr[y] == delcost:
+                (n_s, n_d, n_i) = prev_ops[y]
+                curr_ops[y] = (n_s, n_d + 1, n_i)
             else:
-                cost = 1
-
-            deletion = distance_matrix[i - 1][j] + 1
-            insertion = distance_matrix[i][j - 1] + 1
-            substitution = distance_matrix[i - 1][j - 1] + cost
-
-            min_distance = min(deletion, insertion, substitution)
-
-            distance_matrix[i][j] = min_distance
-
-            if min_distance == substitution:
-                subs, ins, dels = op_matrix[i - 1][j - 1]
-                if cost == 1:
-                    subs += 1
-            elif min_distance == insertion:
-                subs, ins, dels = op_matrix[i][j - 1]
-                ins += 1
-            else:  # deletion
-                subs, ins, dels = op_matrix[i - 1][j]
-                dels += 1
-
-            op_matrix[i][j] = (subs, ins, dels)
-
-    substitutions, insertions, deletions = op_matrix[m][n]
-    return distance_matrix[m][n], (substitutions, insertions, deletions)
+                (n_s, n_d, n_i) = curr_ops[y - 1]
+                curr_ops[y] = (n_s, n_d, n_i + 1)
+    return curr[len(v)], curr_ops[len(v)]
 
 def remove_punctuation(text: str) -> str:
     """
@@ -59,8 +37,41 @@ def remove_punctuation(text: str) -> str:
     """
     return ''.join(ch for ch in text if unicodedata.category(ch)[0] != 'P')
 
+def measure_er(self, reference: str, transcription: str, level: str) -> Tuple[int, int, int, int]:
+        """
+        오류율 계산에 필요한 대체, 삭제, 삽입, 일치의 수를 반환합니다.
+        """
+        ref_list = [reference]
+        hyp_list = [transcription]
+
+        total_s, total_i, total_d, total_n = 0, 0, 0, 0
+
+        for n in range(len(ref_list)):
+            if level == 'char':
+                ref_tokens = list(ref_list[n])
+                hyp_tokens = list(hyp_list[n])
+            elif level == 'word':
+                ref_tokens = ref_list[n].split()
+                hyp_tokens = hyp_list[n].split()
+            else:
+                raise ValueError("Invalid level specified. Choose 'char' or 'word'.")
+
+            _, (s, i, d) = levenshtein(hyp_tokens, ref_tokens)
+            total_s += s
+            total_i += i
+            total_d += d
+            total_n += len(ref_tokens)
+
+        substitutions = total_s
+        deletions = total_d
+        insertions = total_i
+        hits = total_n - (substitutions + deletions)
+
+        return hits, substitutions, deletions, insertions
+"""
 def get_unicode_code(text: str) -> str:
-    """
-    유니코드 문자를 '\\uXXXX' 형식으로 변환한 문자열을 반환합니다.
-    """
+    
+    #유니코드 문자를 '\\uXXXX' 형식으로 변환한 문자열을 반환합니다.
+    
     return ''.join(f'\\u{ord(c):04x}' if ord(c) > 127 else c for c in text)
+"""
